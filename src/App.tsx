8ddf5be3d1f2e855340b6b902459e216aa210018
Mark Wetzel <mark@markwetzel.com>
2020-09-23 19:08:34 -0400

    Testing fetch movies via TMDB.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import * as React from 'react';
 
+import MovieResult from './MovieResult';
+import axiosConfig from './axios';
+
 export interface AppProps {}
 
 const App: React.FunctionComponent<AppProps> = () => {
-  return <div>Books</div>;
+  const [movies, setMovies] = React.useState<MovieResult[]>([]);
+
+  React.useEffect(() => {
+    axiosConfig
+      .get(
+        `/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&query=boondock&page=1&include_adult=false`
+      )
+      .then((res) => {
+        const { results: movieResults } = res.data;
+        console.log(movieResults);
+        setMovies(movieResults);
+      });
+  }, []);
+
+  return (
+    <div>
+      Movies
+      {movies.map((movie) => (
+        <article key={movie.id}>{movie.title}</article>
+      ))}
+    </div>
+  );
 };
 
 export default App;
