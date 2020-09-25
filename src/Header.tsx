4ae4df2dced757dd2c1c22af4126db934b779bf6
Mark Wetzel <mark@markwetzel.com>
2020-09-24 20:39:44 -0400

    Center logo and title text.

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
+import css from '@emotion/css/macro';
 import logo from './logo.png';
+import styled from '@emotion/styled/macro';
 
 export interface HeaderProps {
   title: string;
@@ -8,13 +11,28 @@ export interface HeaderProps {
 
 const Header: React.FunctionComponent<HeaderProps> = (props) => {
   const { title } = props;
+
+  const theme = createMuiTheme();
+
+  const Header = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: center;
+
+    /* ${theme.breakpoints.down(
+      'sm'
+    )} {
+      justify-content: space-around;
+    } */
+  `;
+
   return (
-    <header>
-      <img src={logo} alt='Logo' />
+    <Header>
+      <img src={logo} alt='Logo' height='100px' />
       <Typography component='h1' variant='h2'>
         {title}
       </Typography>
-    </header>
+    </Header>
   );
 };
 
