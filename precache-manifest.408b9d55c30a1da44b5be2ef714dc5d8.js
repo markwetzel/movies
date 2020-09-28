421c471fad7c0bcde546232f5fb5380528d52612
Mark Wetzel <mark@markwetzel.com>
2020-09-28 14:17:36 -0400

    Updates

---
+++ precache-manifest.408b9d55c30a1da44b5be2ef714dc5d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f228c89b126639f80e8ac12e18dc630",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "3e536d54f4112896670e",
+    "url": "/movies/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aec4182ba0b9a891a5cc",
+    "url": "/movies/static/js/2.a34a579d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movies/static/js/2.a34a579d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e536d54f4112896670e",
+    "url": "/movies/static/js/main.74ffb809.chunk.js"
+  },
+  {
+    "revision": "45f1b12226fb8957f395",
+    "url": "/movies/static/js/runtime-main.6ee545b8.js"
+  },
+  {
+    "revision": "a6922b8d8bfc5972adb3828c33451b85",
+    "url": "/movies/static/media/logo.a6922b8d.png"
+  }
+]);
