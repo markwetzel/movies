6892a84a4054da3e436dd1111892d8f3f2045886
Mark Wetzel <mark@markwetzel.com>
2020-09-29 14:59:04 -0400

    Persist watch later to LS.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -39,6 +39,19 @@ const App: React.FunctionComponent<AppProps> = () => {
       .catch((error) => {
         console.log(error);
       });
+
+    // Get current movies from localStorage
+    const currentWatchLaterRaw = localStorage.getItem('watch-later');
+
+    let currentWatchLaterJson;
+
+    if (currentWatchLaterRaw) {
+      currentWatchLaterJson = JSON.parse(currentWatchLaterRaw) as MovieResult[];
+    }
+
+    if (currentWatchLaterJson) {
+      setWatchLater(currentWatchLaterJson);
+    }
   }, []);
 
   const handleSearchSubmit = async (
@@ -67,13 +80,25 @@ const App: React.FunctionComponent<AppProps> = () => {
     if (!watchLater.find((movie: MovieResult) => movie.id === movieResultId)) {
       const movieResult = await API.fetchMovie(movieResultId);
       if (movieResult) {
-        setWatchLater(watchLater.concat(movieResult));
+        const newWatchLater = watchLater.concat(movieResult);
+
+        setWatchLater(newWatchLater);
+
+        // Persist to localStorage
+        localStorage.setItem('watch-later', JSON.stringify(newWatchLater));
       }
     }
   };
 
   const handleRemoveWatchLaterClick = (movieResultId: number) => {
-    setWatchLater(watchLater.filter((movie) => movie.id !== movieResultId));
+    const newWatchLater = watchLater.filter(
+      (movie) => movie.id !== movieResultId
+    );
+
+    setWatchLater(newWatchLater);
+
+    // Persist to localStorage
+    localStorage.setItem('watch-later', JSON.stringify(newWatchLater));
   };
 
   const handleRemoveFavoriteClick = (movieResultId: number) => {
